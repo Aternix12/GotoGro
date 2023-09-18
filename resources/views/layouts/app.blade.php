332ce2156534d7695d891eaa0d108222c8748e12
Benty691 <dalebent2000@gmail.com>
2023-09-18 15:50:23 +1000

    re-jigged css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'My Laravel App')</title>
     <!-- Add CSS files here -->
-     <link rel="stylesheet" href="{{ asset('css/main.css') }}"> 
+     <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
     <!--<link rel="stylesheet" href=" {{ asset('css/app.css') }}" /> temporary while I sort linking css issue. -->
    
 </head>
