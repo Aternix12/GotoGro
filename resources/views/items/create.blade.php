36d0f9a962ccb6fddf4f73a6cd4864d90f62a9fb
Benty691 <dalebent2000@gmail.com>
2023-09-18 14:37:45 +1000

    Update Create Item page HTML / css

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,163 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-<html lang="en">
+
+<html>
     <head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Dale Bent" />
         <meta name="keywords" content="HTML5, tags, CSS" />
-        <link rel="stylesheet" href="/Resources/css/app.css" />
+        <link rel="stylesheet" href="/resources/css/app.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
             href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
         />
-        <title>Add Member Page</title>
+        <title>Add Item Page</title>
     </head>
     <body>
         <nav class="header">
-            <h1 class="Title">Add Member</h1>
+            <h1 class="Title">Add Items</h1>
         </nav>
         <div class="content">
             <div class="LeftGrid">
-                <div class="LGwrapper">
-                    <a href="mylk.com" id="Mylk"
-                        ><img src="/App/Webpages/img/mylk.png" alt="Mylk"
-                    /></a>
-                </div>
+                <a href="mylk.com" id="Mylk"
+                    ><img src="/App/Webpages/img/mylk.png" alt="Mylk" />
+                </a>
             </div>
-            <div class="MiddleGrid addMember">
+
+            <div class="MiddleGrid addItem">
                 <div class="MiddleGridContent">
                     <div class="container">
-                        <h1>Add Member</h1>
+                        <h1>Add Grocery Item</h1>
 
-                        <form
-                            action="{{ route('members.store') }}"
-                            method="POST"
-                        >
+                        <form action="{{ route('items.store') }}" method="POST">
                             @csrf
 
                             <div class="form-group">
-                                <label class="formPrompt" for="FirstName"
-                                    >First Name</label
-                                >
+                                <label for="ProductName">Product Name</label>
                                 <input
                                     type="text"
-                                    name="FirstName"
-                                    id="FirstName"
+                                    name="ProductName"
+                                    id="ProductName"
                                     class="form-control"
                                     required
                                 />
                             </div>
 
                             <div class="form-group">
-                                <label class="formPrompt" for="LastName"
-                                    >Last Name</label
-                                >
+                                <label for="Stock">Stock</label>
                                 <input
                                     type="text"
-                                    name="LastName"
-                                    id="LastName"
+                                    name="Stock"
+                                    id="Stock"
                                     class="form-control"
                                     required
                                 />
                             </div>
 
                             <div class="form-group">
-                                <label class="formPrompt" for="MemberStatusID"
-                                    >Member Status</label
-                                >
-                                <select
-                                    name="MemberStatusID"
-                                    id="MemberStatusID"
-                                    class="form-control"
-                                    required
-                                >
-                                    @foreach ($memberStatuses as $status)
-                                    <option
-                                        value="{{ $status->MemberStatusID }}"
-                                    >
-                                        {{ $status->MemberStatus }}
-                                    </option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="formPrompt" for="DateOfBirth"
-                                    >Date of Birth</label
-                                >
-                                <input
-                                    type="date"
-                                    name="DateOfBirth"
-                                    id="DateOfBirth"
-                                    class="form-control"
-                                    required
-                                />
-                            </div>
-
-                            <div class="form-group">
-                                <label class="formPrompt" for="GenderID"
-                                    >Gender</label
-                                >
-                                <select
-                                    name="GenderID"
-                                    id="GenderID"
-                                    class="form-control"
-                                    required
-                                >
-                                    @foreach ($genders as $gender)
-                                    <option value="{{ $gender->GenderID }}">
-                                        {{ $gender->gender_name }}
-                                    </option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="formPrompt" for="Address"
-                                    >Address</label
-                                >
+                                <label for="Price">Price</label>
                                 <input
                                     type="text"
-                                    name="Address"
-                                    id="Address"
+                                    name="Price"
+                                    id="Price"
                                     class="form-control"
                                     required
                                 />
                             </div>
 
                             <div class="form-group">
-                                <label class="formPrompt" for="Phone"
-                                    >Phone</label
-                                >
+                                <label for="Location">Location</label>
                                 <input
-                                    type="tel"
-                                    name="Phone"
-                                    id="Phone"
-                                    class="form-control"
-                                    required
-                                />
-                            </div>
-
-                            <div class="form-group">
-                                <label class="formPrompt" for="Email"
-                                    >Email</label
-                                >
-                                <input
-                                    type="email"
-                                    name="Email"
-                                    id="Email"
+                                    type="text"
+                                    name="Location"
+                                    id="Location"
                                     class="form-control"
                                     required
                                 />
                             </div>
 
                             <button type="submit" class="btn btn-primary">
-                                Add Member
+                                Add Item
                             </button>
                         </form>
                     </div>
@@ -166,5 +91,4 @@
         </div>
     </body>
 </html>
-   
 @endsection
