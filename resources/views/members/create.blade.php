de499d713c58892e213277f4642d2e3e5794d571
Benty691 <dalebent2000@gmail.com>
2023-09-18 15:34:39 +1000

    trying to get CSS working

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
+ @section(content')
         <nav class="header">
             <h1 class="Title">Add Member</h1>
         </nav>
@@ -147,8 +147,8 @@
                 </div>
             </div>
         </div>
+@endsection
 
-        @endsection
 
 
 
