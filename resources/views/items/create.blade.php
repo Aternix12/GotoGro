4da02d8c02a9e86aa804255c2be578b7e0185358
Benty691 <dalebent2000@gmail.com>
2023-09-19 11:10:55 +1000

    Push new CSS format

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,8 +1,18 @@
-@extends('layouts.app')
-
-@section('content')
-
-
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+        />
+        <title>Member View Page</title>
+    </head>
+    <body>
         <nav class="header">
             <h1 class="Title">Add Items</h1>
         </nav>
@@ -73,5 +83,6 @@
                 </div>
             </div>
         </div>
+    </body>
+</html>
 
-@endsection
