367e28fb55bf26953e4614980aaac784ef423dbb
Tristan Lee Mitchell <tristanmitchell2113@gmail.com>
2023-10-04 18:47:14 +1100

    Departments Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(GenderSeeder::class);
         $this->call(MemberStatusSeeder::class);
         $this->call(OrderStatusSeeder::class);
+        $this->call(DepartmentsSeeder::class);
     }
 }
