cb9f6e2955237bea461bf90efd730ae1cb294820
IamScrump <100543055+IamPeachtree@users.noreply.github.com>
2023-10-09 16:40:55 +1100

    More validation work.
    1 hour. page might be a bit choppy as I am still working on this validation. I think I need to do some work in the regex at the moment but will need some help on the error message handling

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -10,11 +10,13 @@
             <div class="form-group">
                 <label for="FirstName">First Name</label>
                 <input type="text" name="FirstName" id="FirstName" class="form-control" required>
+                <span id="first_name_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="LastName">Last Name</label>
                 <input type="text" name="LastName" id="LastName" class="form-control" required>
+                <span id="last_name_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
@@ -29,6 +31,7 @@
             <div class="form-group">
                 <label for="DateOfBirth">Date of Birth</label>
                 <input type="date" name="DateOfBirth" id="DateOfBirth" class="form-control" required>
+                <span id="date_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
@@ -43,19 +46,65 @@
             <div class="form-group">
                 <label for="Address">Address</label>
                 <input type="text" name="Address" id="Address" class="form-control" required>
+                <span id="address_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="Phone">Phone</label>
                 <input type="tel" name="Phone" id="Phone" class="form-control" required>
+                <span id="phone_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="Email">Email</label>
                 <input type="email" name="Email" id="Email" class="form-control" required>
+                <span id="email_error" class ="error_message"></span>
             </div>
 
             <button type="submit" class="btn btn-primary">Create</button>
         </form>
     </div>
+
+
+    <script>
+
+        //for the display of validation errors
+        function errorMsg(errors){ 
+            Object.keys(errors).forEach(fieldName =>{
+                const errorContainer = document.getElementById(`${name}_error`);
+                if(errorContainer){
+                    errorContainer.innerText = errors[fieldName][0];
+                }
+            });
+        }
+
+        document.getElementById('memberForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const formData = new FormData(event.target);
+            //access controller
+            // ignore the error here. page still works
+            fetch('{{ route('members.store') }}', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.errors) {
+                    // Display errors next to form fields
+                    errorMsg(data.errors);
+                } else {
+                    // Handle success (redirect, show success message, etc.)
+                    alert(data.message);
+                    // access controller
+                    // ignore the error here. page still works
+                    window.location.href = '{{ route('members.index') }}';
+                }
+            })
+            .catch(error => {
+                // Handle other errors
+                console.error('Error:', error);
+            });
+        });
+    </script>
 @endsection
