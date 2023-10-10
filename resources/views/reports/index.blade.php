9e8a57557eb41517211de88c271da315237be268
benty691 <144002502+benty691@users.noreply.github.com>
2023-10-10 23:50:14 +1100

    Essentially finished HTML / CSS for report page

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -6,11 +6,10 @@
         <div class="header-wrapper">
         <h1 class="gen-report">Generate report</h1>
         <form action="">
-            <input type="date" id = "date">
+            <input type="date" id = "dateReport">
         </form>
         </div>
         <div class="space-div"></div>
-        <h2 id= "gen-head">Included details:</h2>
         <a href="{{ route('members.index') }}" class= "report-blocks">
             <div class="form-group-gen" >
                 <p class="internal-group">Members</p>
@@ -35,9 +34,125 @@
             </div>
 
         </a>
+        <h2 class="GotoGro">Goto-Gro MRM</h2>
+        <div class="reportSectionTopWrapper">
+            <div class="reportSectionTop">
+                <p class="reportData">
+                    Filler
+                </p>
+                <p class="reportDataBottom">
+                    Filler but longer
+                </p>
+            </div>
 
+            <div class="reportSectionTop">
+                <p class="reportData">
+                    Filler
+                </p>
+                <p class="reportDataBottom">
+                    Filler but longer
+                </p>
+            </div>
+
+            <div class="reportSectionTop">
+                <p class="reportData">
+                    Filler
+                </p>
+                <p class="reportDataBottom">
+                    Filler but longer
+                </p>
+            </div>
+        </div>
+        <div class="reportSectionLowerWrapper">
+            <div class="reportData2">
+                <p class="reportDataForecast">
+                    Trending products filler 
+                </p>
+                <p class="reportDataForecast2">
+                    Inventory forecast
+                </p>
+            </div>
+            <div class="reportData2">
+            <p class="reportDataForecast">
+                    Trending products filler 
+                </p>
+                <p class="reportDataForecast2">
+                    Inventory forecast
+                </p>
+            </div>
+        </div>
+        <div class="trendingProducts">  
+            <h2 class="GotoGro">
+                 Goto-Gro MRM
+            </h2>
+            <h2 id="gen-head" >
+                Trending Products
+            </h2>
+        </div>
+        <div class="trendingGrid">
+            <div class="left-Grid">
+                <div class="productItem">
+                   <p class="items">Test</p>
+                   <p class="items">Low</p>
+
+                </div>
+                <div class="productItem">
+                <p class="items">Test</p>
+                   <p class="items">Low</p>
+                    
+                </div>
+                <div class="productItem">
+                <p class="items">Test</p>
+                   <p class="items">Low</p>
+                    
+                </div>
+            </div>
+            <div class="center-Grid">
+                <div class="productItem">
+                <p class="items">Test</p>
+                   <p class="items">Low</p>
+                    
+                </div>
+                <div class="productItem">
+                <p class="items">Test</p>
+                <p class="items">Low</p>
+                    
+                </div>
+                <div class="productItem">
+                <p class="items">Test</p>
+                   <p class="items">Low</p>
+                    
+                </div>
+            </div>
+            <div class="right-Grid">
+                <h3 class = "status">Med</h3>
+                <div class="productItem">
+                <p class="items">Test</p>
+                   <p class="items">Low</p>
+                    
+                </div>
+                <h3 class = "status">High</h3>
+                <div class="productItem">
+                <p class="items">Test</p>
+                   <p class="items">Low</p>
+                    
+                </div>
+                <h3 class = "status">Low</h3>
+                <div class="productItem">
+
+                <p class="items">Test</p>
+                <p class="items">Low</p>
+                    
+                </div>
+            </div>
+        </div>
+
+
+
+        </div>
     </div>
+
     <button id="gen-button">
             Generate
-        </button>
+    </button>
 @endsection
