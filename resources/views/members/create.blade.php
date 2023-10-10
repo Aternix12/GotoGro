232d366b025a90de95cdf2ab0c478d12cf6d40aa
elysiacodes <43836238+elysiacodes@users.noreply.github.com>
2023-10-10 18:46:55 +1100

    validation showing for member, for riley to finish

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -9,19 +9,21 @@
 
             <div class="form-group">
                 <label for="FirstName">First Name</label>
-                <input type="text" name="FirstName" id="FirstName" class="form-control" required>
-                <span id="first_name_error" class ="error_message"></span>
+                <input type="text" name="FirstName" id="FirstName" class="form-control {{ $errors->has('FirstName') ? 'is-invalid' : '' }}" novalidate>
+                @error('FirstName')
+                    <span id="first_name_error" class ="error_message">{{ $message }}</span>
+                @enderror
             </div>
 
             <div class="form-group">
                 <label for="LastName">Last Name</label>
-                <input type="text" name="LastName" id="LastName" class="form-control" required>
+                <input type="text" name="LastName" id="LastName" class="form-control" novalidate>
                 <span id="last_name_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="MemberStatusID">Member Status</label>
-                <select name="MemberStatusID" id="MemberStatusID" class="form-control" required>
+                <select name="MemberStatusID" id="MemberStatusID" class="form-control" novalidate>
                     @foreach ($memberStatuses as $status)
                         <option value="{{ $status->MemberStatusID }}">{{ $status->MemberStatus }}</option>
                     @endforeach
@@ -30,13 +32,13 @@
 
             <div class="form-group">
                 <label for="DateOfBirth">Date of Birth</label>
-                <input type="date" name="DateOfBirth" id="DateOfBirth" class="form-control" required>
+                <input type="date" name="DateOfBirth" id="DateOfBirth" class="form-control" novalidate>
                 <span id="date_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="GenderID">Gender</label>
-                <select name="GenderID" id="GenderID" class="form-control" required>
+                <select name="GenderID" id="GenderID" class="form-control" novalidate>
                     @foreach ($genders as $gender)
                         <option value="{{ $gender->GenderID }}">{{ $gender->gender_name }}</option>
                     @endforeach
@@ -45,19 +47,19 @@
 
             <div class="form-group">
                 <label for="Address">Address</label>
-                <input type="text" name="Address" id="Address" class="form-control" required>
+                <input type="text" name="Address" id="Address" class="form-control" novalidate>
                 <span id="address_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="Phone">Phone</label>
-                <input type="tel" name="Phone" id="Phone" class="form-control" required>
+                <input type="tel" name="Phone" id="Phone" class="form-control" novalidate>
                 <span id="phone_error" class ="error_message"></span>
             </div>
 
             <div class="form-group">
                 <label for="Email">Email</label>
-                <input type="email" name="Email" id="Email" class="form-control" required>
+                <input type="email" name="Email" id="Email" class="form-control" novalidate>
                 <span id="email_error" class ="error_message"></span>
             </div>
 
@@ -66,7 +68,7 @@
     </div>
 
 
-    <script>
+    <!-- <script>
 
         //for the display of validation errors
         function errorMsg(errors){ 
@@ -106,5 +108,5 @@
                 console.error('Error:', error);
             });
         });
-    </script>
+    </script> -->
 @endsection
